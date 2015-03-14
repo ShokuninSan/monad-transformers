fb28e749ba308f53dbf3eb5b5a6f4cb12ba70754
Shokunin San <me@flatmap.io>
2015-03-14 23:00:51 +0100

    Ex5 Creating a typeclass.
    Create a type class `Serializable`, a function `toBytes` that impicitly uses this
    typeclass, and instances for `Int` and `String`.

--- basics/src/main/scala/flatten/Part09.scala
+++ basics/src/main/scala/flatten/Part09.scala
@@ -8,5 +8,18 @@ trait Part09 {
 
   // Exercise: without looking at the previous part, create a type class `Serializable`, a function `toBytes` that impicitly uses this
   // typeclass, and instances for `Int` and `String`.
+  trait Serializable[A] {
+    def serialize(value: A): Array[Byte]
+  }
+
+  def toBytes[A:Serializable](a: A): Array[Byte] = implicitly[Serializable[A]].serialize(a)
+
+  implicit object MyIntSerializable extends Serializable[Int] {
+    override def serialize(value: Int): Array[Byte] = value.toString.getBytes
+  }
+
+  implicit object MyStringSerializable extends Serializable[String] {
+    override def serialize(value: String): Array[Byte] = value.getBytes
+  }
 
 }
